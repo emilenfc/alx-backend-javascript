b64c2b900e2de22439a1e3e772769f20bb7c814a
IYADUKUNZE Emile <80049630+emilenfc@users.noreply.github.com>
2022-12-26 12:18:49 +0200

    error fixed

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,3 +1,4 @@
+
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -10,6 +11,7 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
 
