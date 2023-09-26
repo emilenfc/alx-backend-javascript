b111e373d700c3ff4dc1dabe0d5f7e8d257ec162
IYADUKUNZE Emile <iyaemile4@gmail.com>
2023-09-27 00:38:23 +0200

    create employees object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const department in employeesList) {
+    if (Object.prototype.hasOwnProperty.call(employeesList, department)) {
+      allEmployees[department] = [...employeesList[department]];
+    }
+  }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
